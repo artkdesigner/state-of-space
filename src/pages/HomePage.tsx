c090cfad8e31051a766ce2cf0768b518e2a47642
Artem Kalinin <art.kalinin.design@gmail.com>
2026-09-02 20:26:51 +0000

    Build Intro section from Figma (desktop/tablet/mobile)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import HeroSection from '../components/HeroSection'
+import IntroSection from '../components/IntroSection'
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
+      <IntroSection />
     </main>
   )
 }
